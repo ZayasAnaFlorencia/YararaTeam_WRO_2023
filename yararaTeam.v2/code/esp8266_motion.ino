eac12ae5a505927fecea0174760030c9d1edd9ef
jjcampis <jjcampis@gmail.com>
2023-11-01 13:52:13 -0300

    corrected code

--- yararaTeam.v2/code/esp8266_motion.ino
+++ yararaTeam.v2/code/esp8266_motion.ino
@@ -1,38 +1,35 @@
 #include <Servo.h>
-
+//
 Servo servoMotor;
-int ma = 1;
-int mpwm = 3;
-int servoPin = 5;
+int ma = 4;
+int mpwm = 2;
+int servoPin = 14;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); // Inicializa la comunicación serial a 9600 baudios
   servoMotor.attach(servoPin);
-
+  //
   pinMode(ma, OUTPUT);
-  pinMode(mb, OUTPUT);
   pinMode(mpwm, OUTPUT);
 }
 
 void loop() {
   if (Serial.available() > 0) {
-    String data = Serial.readStringUntil('\n');
-
-    int separatorIndex = data.indexOf(',');
-    if (separatorIndex != -1) {
-      String servoData = data.substring(0, separatorIndex);
-      String motorData = data.substring(separatorIndex + 1);
-
-
-      int servoValue = servoData.toInt();
-      int motorValue = motorData.toInt();
-      Serial.print(servoValue);
-      Serial.print("  ");
-      Serial.println(motorValue);
-      Direccion(servoValue);
-      Motor(motorValue);
-    }
+    String palabra = Serial.readStringUntil('\n'); // Lee una palabra hasta que se reciba una nueva línea
+    int separatorIndex = palabra.indexOf(',');
+    
+  if (separatorIndex != -1) {
+    String servoData = palabra.substring(0, separatorIndex);
+    String motorData = palabra.substring(separatorIndex + 1);
+    int servoValue = servoData.toInt();
+    int motorValue = motorData.toInt();
+    Serial.print(servoValue);
+    Serial.print("  ");
+    Serial.println(motorValue);
+    Direccion(servoValue);
+    Motor(motorValue);
   }
+ }
 }
 
 void Direccion(int servoValue) {
@@ -40,7 +37,6 @@ void Direccion(int servoValue) {
 }
 
 void Motor(int motorValue) {
-
   digitalWrite(ma, HIGH);
   analogWrite(mpwm, motorValue);
 }
